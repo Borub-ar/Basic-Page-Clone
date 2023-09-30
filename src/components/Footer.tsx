ddd3b7b3adfd53a93286422e5521f92082c96051
Kacper <barabasz64@gmail.com>
2023-09-30 22:07:01 +0200

    Add basic components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div></div>
+      <div></div>
+
+      <div></div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Footer from '../../components/Footer';
+import styles from './Home.module.css';
+
+const Home = () => {
+  return (
+    <main className={styles.homeWrapper}>
+      <Footer />
+    </main>
+  );
+};
+
+export default Home; 
