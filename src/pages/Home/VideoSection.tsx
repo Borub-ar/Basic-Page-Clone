fab2874ce71eb8ce81594ae2c0e02a1d6d1e0538
Kacper <barabasz64@gmail.com>
2024-05-11 13:58:28 +0200

    Add custom scrollbar, edit Video section

--- src/pages/Home/VideoSection.tsx
+++ src/pages/Home/VideoSection.tsx
@@ -1,26 +1,23 @@
 import { useEffect, useRef, useContext } from 'react';
 import PortalContext from '../../store/portal-context';
 import styles from './VideoSection.module.css';
+
 import HoverButton from '../../components/Buttons/HoverLinkButton';
 
 const VideoSection = () => {
-  const sectionRef = useRef(null);
+  const sectionRef = useRef<HTMLTableSectionElement>(null);
   const portalContext = useContext(PortalContext);
 
   useEffect(() => {
     const options = {
       root: document.querySelector('#scrollArea'),
       rootMargin: '100px',
-      threshold: 0.9,
+      threshold: 0.7,
     };
 
     const callback = (entries: IntersectionObserverEntry[]) => {
       entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          portalContext.changePageTheme(true);
-        } else {
-          portalContext.changePageTheme(false);
-        }
+        portalContext.changePageTheme(entry.isIntersecting);
       });
     };
 
@@ -35,14 +32,22 @@ const VideoSection = () => {
 
   return (
     <section className={styles.wrapper} ref={sectionRef}>
-      <div className={styles.videoPlaceholder}></div>
+      <video preload='metadata' autoPlay>
+        <source
+          src='https://cdn.sanity.io/files/8nn8fua5/production/e4a840ba8dfeded08ac4d0ba6e930be56fc68e3b.mp4'
+          type='video/mp4'
+        />
+      </video>
 
-      <div className={styles.textWrapper}>
-        <h2 className={styles.header}>BASIC/DEPT® HELPS BRANDS ● CONNECT W/ CULTURE</h2>
-        <p>
-          ADWEEK <span>AGENCY SPOTLIGHT</span>
-        </p>
-        <HoverButton href='#'>ABOUT US</HoverButton>
+      {/* Empty div serves as wrapper for sticky element */}
+      <div>
+        <div className={styles.textWrapper}>
+          <h2 className={styles.header}>BASIC/DEPT® HELPS BRANDS ● CONNECT W/ CULTURE</h2>
+          <p>
+            ADWEEK <span>AGENCY SPOTLIGHT</span>
+          </p>
+          <HoverButton href='#'>ABOUT US</HoverButton>
+        </div>
       </div>
     </section>
   );
